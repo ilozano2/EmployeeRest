20b95f82afc80c23f1737a6195886f5aa4617e18
Agustín Ventura <agustinventura@gmail.com>
2017-03-26 13:43:09 +0200

    changes hooks to spring boot guide ones

--- src/main/java/es/aguasnegras/EmployeerestApplication.java
+++ src/main/java/es/aguasnegras/EmployeerestApplication.java
@@ -7,10 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeerestApplication {
 
-    private static Logger logger = Logger.getLogger(EmployeerestApplication.class);
-
     public static void main(String[] args) {
 		SpringApplication.run(EmployeerestApplication.class, args);
-		logger.info("App init");
 	}
 }
