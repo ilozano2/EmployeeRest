77fb17f9aae72b9df51902ee21d22fa70ad25254
Agustín Ventura <agustinventura@gmail.com>
2017-03-26 13:35:55 +0200

    adds log to verifiy app init

--- src/main/java/es/aguasnegras/EmployeerestApplication.java
+++ src/main/java/es/aguasnegras/EmployeerestApplication.java
@@ -1,12 +1,16 @@
 package es.aguasnegras;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeerestApplication {
 
-	public static void main(String[] args) {
+    private static Logger logger = Logger.getLogger(EmployeerestApplication.class);
+
+    public static void main(String[] args) {
 		SpringApplication.run(EmployeerestApplication.class, args);
+		logger.info("App init");
 	}
 }
